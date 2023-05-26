fd1f29896ae4dacb7df68022c77f9f95670d69ce
Alex Kuperwood <noblethrow@gmail.com>
2023-05-26 12:30:18 +0100

    Attribute exceptions fix

--- src/Exception/EntityFactoryException.php
+++ src/Exception/EntityFactoryException.php
@@ -14,8 +14,6 @@ use Exception;
 class EntityFactoryException extends Exception
 {
     public const UNDEFINED_ATTRIBUTE_ARRAY = 'Attribute array must be provided!';
-    public const UNDEFINED_ATTRIBUTE_NAME = 'Attribute name must be provided!';
-    public const UNDEFINED_ATTRIBUTE_TYPE = 'Attribute type must be provided!';
 
     /**
      * @throws EntityFactoryException
@@ -23,21 +21,5 @@ class EntityFactoryException extends Exception
     public static function undefinedAttributeArray()
     {
         throw new static(self::UNDEFINED_ATTRIBUTE_ARRAY);
-    }
-
-    /**
-     * @throws EntityFactoryException
-     */
-    public static function undefinedAttributeName()
-    {
-        throw new static(self::UNDEFINED_ATTRIBUTE_NAME);
-    }
-
-    /**
-     * @throws EntityFactoryException
-     */
-    public static function undefinedAttributeType()
-    {
-        throw new static(self::UNDEFINED_ATTRIBUTE_TYPE);
     }
 }
--- tests/Eav/EntityFactoryException/EntityFactoryExceptionFunctionalTest.php
+++ tests/Eav/EntityFactoryException/EntityFactoryExceptionFunctionalTest.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 
 namespace Tests\Eav\EntityFactoryException;
 
-use Drobotik\Eav\Exception\AttributeTypeException;
 use Drobotik\Eav\Exception\EntityFactoryException;
 use PHPUnit\Framework\TestCase;
 
@@ -26,26 +25,5 @@ class EntityFactoryExceptionFunctionalTest extends TestCase
         $this->expectExceptionMessage(EntityFactoryException::UNDEFINED_ATTRIBUTE_ARRAY);
         EntityFactoryException::undefinedAttributeArray();
     }
-    /**
-     * @test
-     * @group functional
-     * @covers \Drobotik\Eav\Exception\EntityFactoryException::undefinedAttributeArray
-     */
-    public function attribute_name_not_provided()
-    {
-        $this->expectException(EntityFactoryException::class);
-        $this->expectExceptionMessage(EntityFactoryException::UNDEFINED_ATTRIBUTE_NAME);
-        EntityFactoryException::undefinedAttributeName();
-    }
-    /**
-     * @test
-     * @group functional
-     * @covers \Drobotik\Eav\Exception\EntityFactoryException::undefinedAttributeType
-     */
-    public function attribute_type_not_provided()
-    {
-        $this->expectException(EntityFactoryException::class);
-        $this->expectExceptionMessage(EntityFactoryException::UNDEFINED_ATTRIBUTE_TYPE);
-        EntityFactoryException::undefinedAttributeType();
-    }
+
 }
