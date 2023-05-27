f1eb81467feb5f49f0e9fab659058fca98752451
Alex Kuperwood <noblethrow@gmail.com>
2023-05-27 17:52:05 +0100

    move method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,6 +10,9 @@ declare(strict_types=1);
 namespace Tests;
 
 use Drobotik\Eav\Database\Connection;
+use Drobotik\Eav\Enum\_ATTR;
+use Drobotik\Eav\Enum\ATTR_FACTORY;
+use Drobotik\Eav\Enum\ATTR_TYPE;
 use Drobotik\Eav\Factory\EavFactory;
 use Faker\Generator;
 use Illuminate\Database\Capsule\Manager as Capsule;
@@ -49,6 +52,47 @@ class TestCase extends \PHPUnit\Framework\TestCase
         $migrator->rollback();
         Cleaner::run();
         parent::tearDown();
+    }
+
+    protected function getFactoryDefaultConfig(): array
+    {
+        return [
+            ATTR_TYPE::STRING->value() => [
+                ATTR_FACTORY::ATTRIBUTE->field() => [
+                    _ATTR::NAME->column() => ATTR_TYPE::STRING->value(),
+                    _ATTR::TYPE->column() => ATTR_TYPE::STRING->value(),
+                    _ATTR::DEFAULT_VALUE->column() => ATTR_TYPE::STRING->randomValue(),
+                ]
+            ],
+            ATTR_TYPE::INTEGER->value() => [
+                ATTR_FACTORY::ATTRIBUTE->field() => [
+                    _ATTR::NAME->column() => ATTR_TYPE::INTEGER->value(),
+                    _ATTR::TYPE->column() => ATTR_TYPE::INTEGER->value(),
+                    _ATTR::DEFAULT_VALUE->column() => ATTR_TYPE::INTEGER->randomValue(),
+                ]
+            ],
+            ATTR_TYPE::DECIMAL->value() => [
+                ATTR_FACTORY::ATTRIBUTE->field() => [
+                    _ATTR::NAME->column() => ATTR_TYPE::DECIMAL->value(),
+                    _ATTR::TYPE->column() => ATTR_TYPE::DECIMAL->value(),
+                    _ATTR::DEFAULT_VALUE->column() => ATTR_TYPE::DECIMAL->randomValue(),
+                ]
+            ],
+            ATTR_TYPE::DATETIME->value() => [
+                ATTR_FACTORY::ATTRIBUTE->field() => [
+                    _ATTR::NAME->column() => ATTR_TYPE::DATETIME->value(),
+                    _ATTR::TYPE->column() => ATTR_TYPE::DATETIME->value(),
+                    _ATTR::DEFAULT_VALUE->column() => ATTR_TYPE::DATETIME->randomValue(),
+                ]
+            ],
+            ATTR_TYPE::TEXT->value() => [
+                ATTR_FACTORY::ATTRIBUTE->field() => [
+                    _ATTR::NAME->column() => ATTR_TYPE::TEXT->value(),
+                    _ATTR::TYPE->column() => ATTR_TYPE::TEXT->value(),
+                    _ATTR::DEFAULT_VALUE->column() => ATTR_TYPE::TEXT->randomValue(),
+                ]
+            ]
+        ];
     }
 
 }
