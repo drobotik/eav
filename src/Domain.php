07171bb27d1f3faeaa98be20648b0c4230e6547f
drobotik <noblethrow@gmail.com>
2025-03-14 18:45:25 +0000

    working with dependencies vol.3

--- src/Domain.php
+++ src/Domain.php
@@ -10,44 +10,7 @@ declare(strict_types=1);
 
 namespace Drobotik\Eav;
 
-use Drobotik\Eav\Export\ExportManager;
-use Drobotik\Eav\Import\ImportManager;
-use Drobotik\Eav\Result\Result;
-
 class Domain
 {
-    private ExportManager $exportManager;
-    private ImportManager $importManager;
 
-    public function getExportManager() : ExportManager
-    {
-        return $this->exportManager;
-    }
-
-    public function setExportManager(ExportManager $manager) : void
-    {
-        $manager->setDomain($this);
-        $this->exportManager = $manager;
-    }
-
-    public function getImportManager() : ImportManager
-    {
-        return $this->importManager;
-    }
-
-    public function setImportManager(ImportManager $manager) : void
-    {
-        $manager->setDomain($this);
-        $this->importManager = $manager;
-    }
-
-    public function import(): Result
-    {
-        return $this->getImportManager()->run();
-    }
-
-    public function export(int $domainKey, int $setKey, array $filters, array $columns): Result
-    {
-        return $this->getExportManager()->run($domainKey, $setKey, $filters, $columns);
-    }
 }
